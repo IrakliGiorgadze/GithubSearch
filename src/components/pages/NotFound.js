dac2ee6774be19693f0a4c5bfa2421e9e4443ae5
IrakliGiorgadze <ika.giorgadze@gmail.com>
2020-03-30 21:39:32 +0400

    add notfound

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Not Found, Sorry...</h1>
+            <p className="lead"><a href="/">Please go back to Home Page</a></p>
+        </div>
+    )
+}
+
+export default NotFound
